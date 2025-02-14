573d80af9a798be72e88caa9029010fe94b4e907
Abhinav pandey <abhinav@VNSOFT.COM>
2025-02-14 14:32:00 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,16 +6,10 @@ app = Flask(__name__)
 def home():
     return "Hello, World! Welcome to My Website."
 
-if __name__ == '__main__':
-    app.run(debug=True)
     
     
- from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
+@app.route('/index')
+def loan():
     return render_template('index.html')
     
 @app.route('/about')
