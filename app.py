49af8703e6fd69aae4d8eebf82ede3fdddb87b16
Abhinav pandey <abhinav@VNSOFT.COM>
2025-02-19 17:12:36 +0530

    Initial commit of the lightweight website

--- app.py
+++ app.py
@@ -4,19 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def HOMEPAGE():
-    return render_template('HOMEPAGE.html')
+    return render_template('HOME PAGE.html')
 
-    
-    
-@app.route('/index')
-def home():
-    return render_template('index.html')
-    
-@app.route('/about')
-def about():
-    return render_template('about.html')
-    
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
