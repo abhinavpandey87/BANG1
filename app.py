db5bad2ada0506c11e9c3f7afe236f3bf7a01abb
Abhinav pandey <abhinav@VNSOFT.COM>
2025-02-19 16:58:40 +0530

    Initial commit of the lightweight website

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, World! Welcome to My Website."
+def HOMEPAGE():
+    return render_template('HOMEPAGE.html')
 
     
     
@@ -15,6 +15,8 @@ def loan():
 @app.route('/about')
 def about():
     return render_template('about.html')
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
